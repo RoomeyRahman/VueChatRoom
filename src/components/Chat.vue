f3f919d6a4f9c29e449c82221ecb5caf57103274
Roomey Rahman <roomeyrahman@gmail.com>
2020-04-03 21:43:14 +0600

    ordering chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -4,10 +4,10 @@
     <div class="card">
        <div class="card-content">
          <ul class="messages">
-           <li>
-             <span class="teal-text">Name</span>
-             <span class="grey-text text-darken-3">message</span>
-             <span class="grey-text time">time</span>
+           <li v-for="message in messages" :key="message.id">
+             <span class="teal-text">{{ message.name }}</span>
+             <span class="grey-text text-darken-3">{{ message.content }}</span>
+             <span class="grey-text time">{{ message.timestamp }}</span>
            </li>
          </ul>
        </div>
@@ -19,7 +19,8 @@
 </template>
 
 <script>
-import NewMessage from '@/components/NewMessage'
+import NewMessage from "@/components/NewMessage";
+import db from "@/firebase/init";
 
 export default {
     name: "Chat",
@@ -27,8 +28,25 @@ export default {
     components: { NewMessage },
     data () {
         return {
-
+            messages: []
         };
+    },
+    created () {
+        let ref = db.collection("messages").orderBy('timestamp');
+
+        ref.onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(changes => {
+                if (changes.type == "added") {
+                    let doc = changes.doc;
+                    this.messages.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        content: doc.data().content,
+                        timestamp: doc.data().timestamp
+                    });
+                }
+            });
+        });
     },
     methods: {
 
